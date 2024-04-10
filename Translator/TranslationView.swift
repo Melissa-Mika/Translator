69f639e22b395b7ee60b403512864fd9c59cf3a4
Jordan <jordan@nsa.icu>
2024-04-10 10:32:05 -0500

    TranslationView, text area, and formatting.

---
+++ Translator/TranslationView.swift
@@ -0,0 +1,18 @@
+//
+//  TranslationView.swift
+//  Translator
+//
+//  Created by Jordan Carter on 4/10/24.
+//
+
+import SwiftUI
+
+struct TranslationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslationView()
+}
