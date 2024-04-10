39f0054e37d9b07ad1d41b6bd8557010cb0c4563
MIKA <mm0970610@dt050565.otc.edu>
2024-04-10 09:53:06 -0500

    initial commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by MIKA, MELISSA M. on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
