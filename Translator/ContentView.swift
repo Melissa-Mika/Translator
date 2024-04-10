c47045ff29604cc38859c2a2e821f9183eba2017
Jordan <jordan@nsa.icu>
2024-04-10 09:58:52 -0500

    Add things

--- Translator/ContentView.swift
+++ Translator/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Translator!")
+                .font(.title)
+                .padding()
+            TextField("English", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
