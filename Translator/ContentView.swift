9af28b80dc7f9176914fe9a8e9b8cc4e31a2934a
Jordan <18247749+1-TB@users.noreply.github.com>
2024-04-10 10:40:10 -0500

    Update ContentView.swift

--- Translator/ContentView.swift
+++ Translator/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var englishText = ""
+    @State var isShowingTranslation = false
     var body: some View {
         VStack {
             
             Text("Translator!")
                 .font(.title)
                 .padding()
-            TextField("English", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            Text("Enter words here:")
+            TextEditor(text: $englishText)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .cornerRadius(4.0)
+            Spacer()
+            Button("Translate"){
+                isShowingTranslation.toggle()
+            }
+            .padding()
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(9.0)
+           
+            
+                
+            
         }
         .padding()
+        .sheet(isPresented: $isShowingTranslation){
+            TranslationView(englishText: $englishText)
+        }
     }
 }
 
